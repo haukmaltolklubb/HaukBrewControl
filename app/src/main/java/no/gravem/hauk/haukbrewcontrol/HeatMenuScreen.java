4c268ff8767ce8ce2dd5a31a8152c7617bac6769
haukmaltolklubb <gtgravem@gmail.com>
2015-01-22 22:30:05 +0100

    Initial commit

---
+++ app/src/main/java/no/gravem/hauk/haukbrewcontrol/HeatMenuScreen.java
@@ -0,0 +1,15 @@
+package no.gravem.hauk.haukbrewcontrol;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by GTG on 06.01.2015.
+ */
+public class HeatMenuScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.heat_menu);
+    }
+}
---
+++ app/src/main/java/no/gravem/hauk/haukbrewcontrol/MashMenuScreen.java
@@ -0,0 +1,16 @@
+package no.gravem.hauk.haukbrewcontrol;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by GTG on 20.01.2015.
+ */
+public class MashMenuScreen extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mash_menu);
+    }
+
+}
