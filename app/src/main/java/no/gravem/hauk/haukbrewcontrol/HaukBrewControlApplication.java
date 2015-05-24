4d9c7145e76f08f5dc3b215810ff3c318208e0a7
Anne-Marit Gravem <amgravem@gmail.com>
2015-05-24 15:00:59 +0200

    Slettet filer vi ikke lengre trenger etter refakturering.

--- app/src/main/java/no/gravem/hauk/haukbrewcontrol/HaukBrewControlApplication.java
+++ app/src/main/java/no/gravem/hauk/haukbrewcontrol/HaukBrewControlApplication.java
@@ -10,39 +10,10 @@ import org.w3c.dom.Document;
  */
 public class HaukBrewControlApplication extends Application {
 
-/**    private Document xmlDocument = null;
-    IBrewControlConnection controlConnection = null;
     private static HaukBrewControlApplication ourInstance = new HaukBrewControlApplication();
 
     public static HaukBrewControlApplication getInstance() {
         return ourInstance;
     }
 
-    public HaukBrewControlApplication() {
-    }
-
-    public Document getXmlDocument(){
-        if(xmlDocument == null){
-            Log.d(this.getClass().getName(), "Using URL connection");
-            controlConnection = new BrewControlConnection();
-            //Log.d(this.getClass().getName(), "Using mocked connection");
-            //controlConnection = new BrewControlConnectionMockup();
-            xmlDocument = controlConnection.getStatusXmlDocument(getApplicationContext());
-        }
-        return xmlDocument;
-    }
-
-    public IBrewControlConnection getBrewControlConnection(){
-        Log.d(this.getClass().getName(), "getBrewControlConnection");
-        if(controlConnection != null)
-            return controlConnection;
-        else{
-            controlConnection = new BrewControlConnection();
-            return controlConnection;
-        }
-    }
-
-    public void setXmlDocument(Document xmlDocument){
-        this.xmlDocument = xmlDocument;
-    }**/
 }
